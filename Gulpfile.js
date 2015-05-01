03305e0da75070ce9314a00574cd06c930e063fe
Egemen Berker Kızılcan <egemen@kenarkose.com>
2015-05-01 12:24:31 +0300

    Switched to phpunit

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,26 @@
 var gulp = require('gulp');
-var phpspec = require('gulp-phpspec');
+var phpunit = require('gulp-phpunit');
 var run = require('gulp-run');
 var notify = require('gulp-notify');
 
 gulp.task('test', function() {
-  gulp.src('spec/**/*.php')
-    .pipe(run('clear'))
-    .pipe(phpspec('', { notify: true }))
-    .on('error', notify.onError({
-      title: 'Red!',
-      message: 'Your tests failed...'
-    }))
-    .pipe(notify(
-      {
-        title: 'Green!',
-        message: 'Your tests succeeded!'
-      }
-    ));
+    gulp.src('phpunit.xml')
+        .pipe(run('clear'))
+        .pipe(phpunit('', { notify: true }))
+        .on('error', notify.onError({
+            title: 'Red!',
+            message: 'Your tests failed...'
+        }))
+        .pipe(notify(
+            {
+                title: 'Green!',
+                message: 'Your tests succeeded!'
+            }
+        ));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['spec/**/*.php'], ['test']);
+    gulp.watch(['tests/**/*.php'], ['test']);
 });
 
 gulp.task('default', ['test', 'watch']);
